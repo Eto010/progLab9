2e606d8330f332febd92b90d2104e307b2e93a07
Eto010 <kirchenkovlad123@gmail.com>
2025-12-18 10:13:42 +0700

    sort

---
+++ README.md
@@ -0,0 +1,3 @@
+**Оценка 3:**
+
+![Alt text](/home/vlad/progLab9/images/capture_20251212111046357.png)
---
+++ sort.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int[] SortQuick(int[] arr)
+{
+    if(arr.Length < 2) { return arr;}
+
+    int support = arr[0];
+
+    int[] left = SortQuick(arr.Where(x => x < support).ToArray());
+    int[] right = SortQuick(arr.Where(x => x > support).ToArray());
+    int[] center = arr.Where(x => x == support).ToArray();
+
+    return left.Concat(center).Concat(right).ToArray();
+}
